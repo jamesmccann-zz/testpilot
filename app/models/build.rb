b7aff60047da652a68b9b0e75e5fbfabc23c589a
James McCann <jmccnz@gmail.com>
2014-03-09 11:32:18 +1300

    Added carrierwave uploads to build for uploading an APK

--- app/models/build.rb
+++ app/models/build.rb
@@ -2,4 +2,8 @@ class Build < ActiveRecord::Base
   
   belongs_to :app
 
+  mount_uploader :apk, APKUploader
+
+  delegates :name, to: app, prefix: "app"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140308022545) do
+ActiveRecord::Schema.define(version: 20140308222614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140308022545) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "apk"
   end
 
 end

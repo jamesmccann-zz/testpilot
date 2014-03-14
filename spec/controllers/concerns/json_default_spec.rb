5421c98ea520934e6f854e0c1369d8510a235f05
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-14 17:21:47 +1300

    Add JSONDefault controller concern

---
+++ spec/controllers/concerns/json_default_spec.rb
@@ -0,0 +1,39 @@
+describe JSONDefault, type: :controller do
+  controller do
+    include JSONDefault
+
+    def index
+      render nothing: true
+    end
+  end
+
+  describe "ensure_default_format" do
+    context "no format" do
+      before { get :index }
+      it { request.format.should eq "json" }
+    end
+
+    context "json format" do
+      before { get :index, format: :json }
+      it { request.format.should eq "json" }
+    end
+
+    context "unknown format" do
+      before do
+        controller.stub(assert_valid_format!: true)
+        get :index, format: :xml
+      end
+
+      it { request.format.should eq 'xml' }
+    end
+  end
+
+  describe "assert_valid_format!" do
+    context "good format" do
+      it { expect { get :index }.to_not raise_error(ActionController::UnknownFormat) }
+    end
+    context "bad format" do
+      it { expect { get :index, format: :xml }.to raise_error(ActionController::UnknownFormat) }
+    end
+  end
+end

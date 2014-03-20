dc01a6f44c27fbe0c94c8387d4c94f6bc6ac7797
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-20 19:07:29 +1300

    Use more standard thumbnail size

--- app/uploaders/application_icon_uploader.rb
+++ app/uploaders/application_icon_uploader.rb
@@ -24,7 +24,7 @@ class ApplicationIconUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [48, 48]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.

ead76c997d77f87700a710e3799fc1e3ad9d2a8b
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-20 15:25:43 +1300

    Authenticate app with HTTP Basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # Prevent CSRF attacks by nullifying the session.
+  # For applications, you may want to use :exception instead.
+  protect_from_forgery with: :null_session
+  before_action :authenticate!
+
+  private
+
+    def authenticate!
+      authenticate_or_request_with_http_basic do |username, password|
+        ENV.fetch(:HTTP_BASIC_USERNAME, 'test') == username &&\
+        ENV.fetch(:HTTP_BASIC_PASSWORD, 'test123') == password
+      end
+    end
 end

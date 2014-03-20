344d49ff08f83e0ef93906d1496acc94df844ea0
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-21 08:33:28 +1300

    Store APK files in private/

--- app/uploaders/apk_uploader.rb
+++ app/uploaders/apk_uploader.rb
@@ -3,7 +3,7 @@ class APKUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "private/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_white_list

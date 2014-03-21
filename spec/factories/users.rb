3c642cfe9bc72d4e33005404e7b76c7a9a0d6b53
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-21 22:04:17 +1300

    Add users unconfirmed trait

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,11 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user-#{n}@testpilotapp.com" }
     password { "testtest123" }
+    confirmed_at { DateTime.now }
+
+    trait :unconfirmed do
+      confirmation_token 'abc123'
+      confirmed_at nil
+    end
   end
 end

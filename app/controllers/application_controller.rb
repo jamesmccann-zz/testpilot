6858bb63d9751540fdb10613c7a78da5d9d88f32
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-22 13:04:17 +1300

    Remove basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by nullifying the session.
   # For applications, you may want to use :exception instead.
   protect_from_forgery with: :null_session
-  # before_action :authenticate!
 
   private
 
@@ -10,10 +9,4 @@ class ApplicationController < ActionController::Base
       new_user_session_path
     end
 
-    def authenticate!
-      authenticate_or_request_with_http_basic do |username, password|
-        ENV.fetch('HTTP_BASIC_USERNAME', 'test') == username &&\
-        ENV.fetch('HTTP_BASIC_PASSWORD', 'test123') == password
-      end
-    end
 end

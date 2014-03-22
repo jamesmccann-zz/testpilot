b3324cd0a4a0f49a086fa0751389825f31b38d6d
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-22 13:04:40 +1300

    Render authentication layout for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For applications, you may want to use :exception instead.
   protect_from_forgery with: :null_session
   before_filter :authenticate_user!, unless: :devise_controller?
+  layout :layout_by_resource
 
   private
+
+    def layout_by_resource
+      devise_controller? ? "authentication" : "application"
+    end
 
     def after_sign_out_path_for(resource)
       new_user_session_path

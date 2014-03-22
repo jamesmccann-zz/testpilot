1c99bc1f0ae921dfa35414b7d624df2aef25a640
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-22 13:02:10 +1300

    Add authentication helper to sign in user for controller spec

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -5,3 +5,11 @@ def sign_up_with(email, password)
   fill_in 'Password', with: password
   click_button 'Sign in'
 end
+
+def sign_in_user(user = FactoryGirl.create(:user))
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = FactoryGirl.create(:user)
+    sign_in user
+  end
+end

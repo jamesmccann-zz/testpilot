8ed69662fad6e15e895c57f26f9bc5acd78bc84f
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-22 13:04:27 +1300

    Authenticate user with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by nullifying the session.
   # For applications, you may want to use :exception instead.
   protect_from_forgery with: :null_session
+  before_filter :authenticate_user!, unless: :devise_controller?
 
   private
 

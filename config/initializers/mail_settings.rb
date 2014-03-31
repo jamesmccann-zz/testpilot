7f0cc6447a84a3b071e7b3e0884dc1c5fd46d50a
Josh McArthur <joshua.mcarthur@gmail.com>
2014-03-31 16:47:30 +1300

    Update email.yml loading logic

--- config/initializers/mail_settings.rb
+++ config/initializers/mail_settings.rb
@@ -1,10 +1,8 @@
-# Redirect mail to Mailcatcher
-unless Rails.env.test?
-  email_settings = YAML.load_file(File.open("#{Rails.root.to_s}/config/email.yml"))[Rails.env.to_s]
-  Rails.application.config.action_mailer.default_url_options = {
-    host: email_settings.delete("url_host") || "localhost:3000"
-  }
+email_settings = YAML.load_file("#{Rails.root.to_s}/config/email.yml")[Rails.env.to_s]
 
-  Rails.application.config.action_mailer.delivery_method = email_settings.delete("delivery_method")
-  Rails.application.config.action_mailer.smtp_settings = email_settings unless email_settings.empty?
-end
+Rails.application.config.action_mailer.default_url_options = {
+  host: email_settings.delete(:url_host) || "localhost:3000"
+}
+
+Rails.application.config.action_mailer.delivery_method = email_settings.delete(:delivery_method)
+Rails.application.config.action_mailer.smtp_settings = email_settings

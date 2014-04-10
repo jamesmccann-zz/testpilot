a59a39752ca6406970a48669e7e85df08605fc79
Josh McArthur <joshua.mcarthur@gmail.com>
2014-04-10 21:34:03 +1200

    Migrate string role field to boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330192350) do
+ActiveRecord::Schema.define(version: 20140410092734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,9 @@ ActiveRecord::Schema.define(version: 20140330192350) do
   create_table "assignments", force: true do |t|
     t.integer  "app_id"
     t.integer  "user_id"
-    t.string   "role",       default: "tester", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "developer",  default: false, null: false
   end
 
   add_index "assignments", ["app_id", "user_id"], name: "index_assignments_on_app_id_and_user_id", unique: true, using: :btree
